65c786861b39b3fc7cc4e48dab0064ae18a23d90
clepix21 <clement.lemaire2107@gmail.com>
2025-10-16 09:35:44 +0200

    fix: corriger le nom du fichier de style pour la page de connexion

--- app/frontend/src/pages/auth/LoginPage.js
+++ app/frontend/src/pages/auth/LoginPage.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import "../../styles/auth.css"
+import "../../styles/Auth.css"
 
 export default function LoginPage() {
   const navigate = useNavigate()
--- app/frontend/src/pages/auth/RegisterPage.js
+++ app/frontend/src/pages/auth/RegisterPage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../../styles/auth.css';
+import '../../styles/Auth.css';
 
 export default function RegisterPage() {
   const navigate = useNavigate();
